8890573158f7d6fc0a9cb86e6a597ac388a9a3f5
kieykouch <ki.ey.kouch@gmail.com>
2016-08-17 11:19:38 -0700

    hover

--- script/custom.js
+++ script/custom.js
@@ -3,3 +3,12 @@ $(function() {
     window.sr = ScrollReveal();
     sr.reveal('.content-section-b');
 });
+
+$(function(){
+    $('.dropdown').hover(function() {
+        $(this).addClass('open');
+    },
+    function() {
+        $(this).removeClass('open');
+    });
+});
